1133b10e31c996c85f25e49975129d7e93717669
biroue isaac <biroueisaac@gmail.com>
2023-03-14 00:11:18 +0100

    Second commit

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Register your models here
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created', )
+    list_display = ('title', 'genre', 'number_in_stock','daily_rate','date_created')
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Movie(models.Model):
@@ -13,3 +17,7 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return self.title
