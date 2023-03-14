0ab934789411aa67a1ac3be818f2ce0a087f5ee9
biroue isaac <biroueisaac@gmail.com>
2023-03-14 03:10:33 +0100

    seventh commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn vidly.wsgi
+
+
--- vidly/settings.py
+++ vidly/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
